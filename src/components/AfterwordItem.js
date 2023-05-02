09ce3431c5b2f821c13fbf3ce49e5c0fc21ff3f6
sojung32 <sojung0320@gmail.com>
2023-05-02 17:52:01 +0900

    후기 팝업 추가

--- src/components/AfterwordItem.js
+++ src/components/AfterwordItem.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import { Modal } from 'antd';
+import React, { useState } from 'react';
 
 const AfterWord = (props) => {
+    const [popupOpen, setPopupOpen] = useState(false);
+
+    const openPopup = () => {
+        setPopupOpen(true);
+    }
+    const closePopup = () => {
+        setPopupOpen(false);
+    }
+
+    const date = props.date ? props.date : 'yyyy.mm.dd';
     return (
         <div className="aw-item">
-            <div className="aw-wrapper">
+            <div className="aw-wrapper" onClick={openPopup}>
                 <div className="aw-image">
                     <img alt="sek" src="https://pbs.twimg.com/media/FufOmIxWIAA_dNm?format=jpg" />
                 </div>
@@ -12,6 +23,14 @@ const AfterWord = (props) => {
                     <span>2023.03.21</span>
                 </div>
             </div>
+            <Modal title={date + ' 후기'} open={popupOpen} onOk={closePopup} onCancel={closePopup} centered footer={null}>
+                <div className="aw-pop-content">
+                    <div className="aw-pop-image">
+                        <img alt="sek" src="https://pbs.twimg.com/media/FufOmIxWIAA_dNm?format=jpg" />
+                    </div>
+                    <p>후기 내용 후기 내용 후기 내용 후기 내용 후기 내용 </p>
+                </div>
+            </Modal>
         </div>
     );
 };
