ea5d999d88785564a82484d27c9a957ba4eb6fb4
yewo2nn16 <80824750+yewon-Noh@users.noreply.github.com>
2023-06-22 17:11:49 +0900

    #22 EventItem 스타일 수정

--- src/components/EventItem.js
+++ src/components/EventItem.js
@@ -1,43 +1,43 @@
 import React from 'react';
-import { Rate } from 'antd';
+import { Rate, Card } from 'antd';
 import { HeartFilled, EnvironmentOutlined, CalendarOutlined, SmileOutlined } from '@ant-design/icons';
 
 import { Link } from 'react-router-dom';
 
+const { Meta } = Card;
+
 const EventItem = (props) => {
-    let thumbnail = props.thumbnail ? props.thumbnail : 'https://pbs.twimg.com/media/FvFhOr5aYAAA88w?format=jpg';
+    let thumbnail = props.thumbnail ? props.thumbnail : 'https://res.cloudinary.com/celebring/image/upload/v1687418615/celeb/FuQWivIaMAIOrMn_jxphha.jpg';
     let id = props.id ? props.id : 1;
     let title = props.title ? props.title : '이벤트명입니다. 생일카페 이벤트명입니다.';
     let date = props.date ? props.date : '23.11.01 ~ 23.11.03';
     let location = props.location ? props.location : '장소(카페상호명)';
     let celebs = props.celebs ? props.celebs : ['서은광(비투비)', '이민혁(비투비)'];
-    let useHeart = props.useHeart ? props.useHeart : false;
-    let heart = props.heart ? props.heart : 1;
 
     return (
         <div className="event-component">
-            <Link to={`/event/${id}`}>
-                {/* <div className="event-image">
-                    <img src={thumbnail} alt="thumbnail"/>
-                </div> */}
-                <div className="event-content">
-                    <span className="event-title">{title}</span>
-                    <p className="event-date"><CalendarOutlined/> {date}</p>
-                    <p className="event-location"><EnvironmentOutlined/> {location}</p>
-                    <p className="event-celeb"><SmileOutlined /> {String(celebs)}</p>
-                </div>
-            </Link>
-            {useHeart ?
-                <div className="event-like">
-                    <Rate 
-                        character={<HeartFilled />} 
-                        count={1} 
-                        defaultValue={heart}
-                        style={{color: 'red'}}
-                    />
-                </div>
-            : null}
+            <Card
+                hoverable 
+                style={{ width: '100%' }}
+            >
+            <Meta
+                title={title}
+                description={
+                <Link to={`/event/${id}`}> 
+                    <div className="event-content">
+                        <p className="event-date"><CalendarOutlined/> {date}</p>
+                        <p className="event-location"><EnvironmentOutlined/> {location}</p>
+                        <p className="event-celeb"><SmileOutlined /> {String(celebs)}</p>
+                    </div>
+                    <div className="event-image">
+                        <img src={thumbnail} alt="thumbnail"/>
+                    </div>
+                </Link>
+                }
+                />
+            </Card>
         </div>
+        
     );
 };
 
